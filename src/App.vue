a8afb2daab45e568e93e8ca9a246cddca03c8b8c
Kausinho <kausinhobrunow@gmail.com>
2022-04-28 14:32:09 -0300

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,36 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
+  <HeaderCarros />
+  <HomeCarros />
+  <FooterCarros />
   <router-view/>
 </template>
 
+<script>
+import HeaderCarros from './components/HeaderCarros.vue';
+import FooterCarros from './components/FooterCarros.vue';
+import HomeCarros from './components/HomeCarros.vue';
+
+export default {
+  components:{
+    HeaderCarros,
+    FooterCarros,
+    HomeCarros
+  }
+}
+</script>
+
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+@import url('https://fonts.googleapis.com/css2?family=Open+Sans:wght@300;400;700&family=Poppins:wght@300;400;700&display=swap');
+  *{
+    font-family: 'Open Sans';
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
 
-nav {
-  padding: 30px;
-}
-
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-nav a.router-link-exact-active {
-  color: #42b983;
-}
+  .container{
+    margin: 0 auto;
+    padding: 0 2%;
+    max-width: 1280px;
+  }
 </style>
