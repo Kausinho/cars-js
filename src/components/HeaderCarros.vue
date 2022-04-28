8668795dbf2cba16eedff028119c8c0eed872986
Kausinho <kausinhobrunow@gmail.com>
2022-04-28 14:32:35 -0300

    Create HeaderCarros.vue

---
+++ src/components/HeaderCarros.vue
@@ -0,0 +1,57 @@
+<template>
+    <nav class="menu">  
+        <div class="container">
+            <h2><router-link to="/">Home</router-link></h2>
+            <ul>
+                <li><router-link to="/carros">Carros</router-link></li>
+                <li><router-link to="/modelos">Modelos</router-link></li>
+            </ul>
+        </div><!--container-->
+  </nav><!--menu-->
+  <router-view />
+</template>
+
+<script>
+export default {
+    name: 'HeaderCarros'
+}
+</script>
+
+<style scoped>
+    nav.menu{
+        background: rgb(21, 23, 71);
+        padding: 20px;
+    }
+    nav.menu .container{
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+    }
+
+    nav.menu h2{
+        font-size: 30px;
+        font-weight: 300;
+    }
+
+    nav.menu h2 a{
+        text-decoration: none;
+        color: white;
+    }
+
+    nav.menu ul{
+        display: flex;
+        list-style-type: none;
+    }
+
+    nav.menu ul li{
+        margin-left: 40px;
+        flex-direction: row;
+    }
+
+    nav.menu li a{
+        text-decoration: none;
+        color: white;
+        text-transform: uppercase;
+        font-weight: 300;
+    }
+</style>
