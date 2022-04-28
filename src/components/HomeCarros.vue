51c4d46f1ddcb976d16bdf3ff3bdef4dbda18546
Kausinho <kausinhobrunow@gmail.com>
2022-04-28 14:33:02 -0300

    Create HomeCarros.vue

---
+++ src/components/HomeCarros.vue
@@ -0,0 +1,21 @@
+<template>
+    <section class="main">
+        <div class="container">
+            <div class="carros">
+                <div class="carros-box">
+                    <h2></h2>
+                </div><!--carros-box-->
+            </div><!--carros-->
+        </div><!--container-->
+    </section><!--main-->
+</template>
+
+<script>
+export default {
+    name: 'HomeCarros'
+}
+</script>
+
+<style scoped>
+
+</style>
