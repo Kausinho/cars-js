f89312eb2a2ebc3f0498e527b1d13e3615c88e46
Kausinho <kausinhobrunow@gmail.com>
2022-04-29 17:24:37 -0300

    Update HeaderCarros.vue

--- src/components/HeaderCarros.vue
+++ src/components/HeaderCarros.vue
@@ -1,7 +1,7 @@
 <template>
     <nav class="menu">  
         <div class="container">
-            <h2><router-link to="/">Home</router-link></h2>
+            <router-link to="/"><h2>Home</h2></router-link>
             <ul>
                 <li><router-link to="/carros">Carros</router-link></li>
                 <li><router-link to="/modelos">Modelos</router-link></li>
@@ -28,14 +28,14 @@ export default {
         align-items: center;
     }
 
-    nav.menu h2{
-        font-size: 30px;
-        font-weight: 300;
-    }
-
-    nav.menu h2 a{
+    nav.menu a{
         text-decoration: none;
         color: white;
+    }
+
+    nav.menu a h2{
+        font-size: 30px;
+        font-weight: 300;
     }
 
     nav.menu ul{
