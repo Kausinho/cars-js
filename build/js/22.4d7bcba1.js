89a433384b96ba59ecf22a9912f22cfbeae56451
Kausinho <kausinhobrunow@gmail.com>
2022-04-29 13:06:38 -0300

    Create 22.4d7bcba1.js

---
+++ build/js/22.4d7bcba1.js
@@ -0,0 +1,2 @@
+"use strict";(self["webpackChunkcars_js"]=self["webpackChunkcars_js"]||[]).push([[22],{7022:function(s,e,n){n.r(e),n.d(e,{default:function(){return i}});var r=n(3396);const c={class:"modelos"};function a(s,e,n,a,t,u){return(0,r.wg)(),(0,r.iD)("div",c)}var t={name:"ModelosView"},u=n(89);const o=(0,u.Z)(t,[["render",a]]);var i=o}}]);
+//# sourceMappingURL=22.4d7bcba1.js.map
