8a3ae2ad0bffc7d5af2f2c017c32304f5b084a7e
Kausinho <kausinhobrunow@gmail.com>
2022-05-03 13:16:39 -0300

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,5 +1,21 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
+import { createApp } from 'vue';
+import App from './App.vue';
+import router from './router';
+import PrimeVue from 'primevue/config';
+import Button from 'primevue/button';
+import InputText from 'primevue/inputtext';
+import Toast from 'primevue/toast';
+import ToastService from 'primevue/toastservice';
 
-createApp(App).use(router).mount('#app')
+import 'primevue/resources/themes/saga-blue/theme.css';
+import 'primevue/resources/primevue.min.css';
+import 'primeicons/primeicons.css';
+
+createApp(App).use(router).mount('#app');
+
+App.use(PrimeVue);
+App.use(ToastService);
+
+App.component('InputText', InputText);
+App.component('Button', Button);
+App.component('Toast', Toast);
